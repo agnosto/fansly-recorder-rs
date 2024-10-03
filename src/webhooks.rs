9083ace04d5e4fac10a6aa273881592804957843
agnosto <agnostoshi@protonmail.com>
2024-10-03 00:54:29 -0700

    update(UNTESTED): Switch from webhook to serenity
    - Adds possibility for file upload in embed
    - More up to date

--- src/webhooks.rs
+++ src/webhooks.rs
@@ -1,7 +1,8 @@
 use crate::config::WebhookConfig;
-use anyhow::Result;
-use webhook::client::WebhookClient;
-//use Chrono::Utc;
+use anyhow::{Context, Result};
+use serenity::all::Colour;
+use serenity::builder::{CreateAttachment, CreateEmbed, ExecuteWebhook};
+use serenity::{http::Http, model::webhook::Webhook};
 
 pub async fn send_live_noti(
     config: &WebhookConfig,
@@ -12,28 +13,33 @@ pub async fn send_live_noti(
         return Ok(());
     }
 
-    let client = WebhookClient::new(&config.live_webhook);
+    let http = Http::new("");
+    let webhook = Webhook::from_url(&http, &config.live_webhook)
+        .await
+        .context("Failed to create webhook")?;
+
     let live_url = format!("https://fansly.com/live/{}", username);
 
-    let _ = client
-        .send(|message| {
-            message
-                .content(&format!(
-                    "{} {} is now live on Fansly!",
-                    config.webhook_mention, username
-                ))
-                .username("Fansly Recorder RS")
-                .embed(|embed| {
-                    embed
-                        .title("Stream Live!")
-                        .color("0x03b2f8")
-                        .url(&live_url)
-                        .author(username, Some(avatar_url.to_string()), None)
-                        .thumbnail(avatar_url)
-                        .timestamp(&chrono::Utc::now().to_rfc3339())
-                })
-        })
-        .await;
+    let embed = CreateEmbed::default()
+        .title(format!("{} is now Live!", username))
+        .color(Colour::from_rgb(3, 178, 248))
+        .url(live_url)
+        //.author(|a: &mut CreateEmbed| a.name(username).icon_url(avatar_url))
+        .thumbnail(avatar_url)
+        .timestamp(chrono::Utc::now());
+
+    let builder = ExecuteWebhook::default()
+        .content(format!(
+            "{} {} is now live on Fansly!",
+            config.webhook_mention, username
+        ))
+        .username("Fansly Recorder RS")
+        .embed(embed);
+
+    webhook
+        .execute(&http, false, builder)
+        .await
+        .context("Failed to execute webhook")?;
 
     println!("[INFO] Sent live notification for {}", username);
     Ok(())
@@ -44,34 +50,42 @@ pub async fn send_upload_notification(
     config: &WebhookConfig,
     mp4_name: &str,
     sheet_name: &str,
-    _sheet_path: &str,
+    sheet_path: &str,
 ) -> Result<()> {
     if !config.enabled {
         return Ok(());
     }
 
-    let client = WebhookClient::new(&config.info_webhook);
+    let http = Http::new("");
+    let webhook = Webhook::from_url(&http, &config.info_webhook)
+        .await
+        .context("Failed to create webhook")?;
 
-    let _ = client
-        .send(|message| {
-            message
-                .content(&format!("{} Vod Uploaded", config.webhook_mention))
-                .username("Fansly Recorder")
-                .embed(|embed| {
-                    embed
-                        .title("Stream Recording Uploaded")
-                        .description(&format!(
-                            "Uploaded {} with contact sheet {}",
-                            mp4_name, sheet_name
-                        ))
-                        .color("0x03b2f8")
-                        .image("attachment://contact_sheet.jpg")
-                        .timestamp(&chrono::Utc::now().to_rfc3339())
-                })
-            //.file("contact_sheet.jpg", sheet_path)
-        })
-        .await;
+    let embed = CreateEmbed::default()
+        .title("Stream Recording Uploaded")
+        .description(format!(
+            "Uploaded {} with contact sheet {}",
+            mp4_name, sheet_name
+        ))
+        .color(Colour::from_rgb(3, 178, 248))
+        .image("attachment://contact_sheet.jpg")
+        .timestamp(chrono::Utc::now());
 
-    println!("[info] Sent upload notification for {}", mp4_name);
+    let file = CreateAttachment::path(sheet_path)
+        .await
+        .context("Failed to create attachment")?;
+
+    let builder = ExecuteWebhook::default()
+        .content(format!("{} Vod Uploaded", config.webhook_mention))
+        .username("Fansly Recorder")
+        .add_file(file)
+        .embed(embed);
+
+    webhook
+        .execute(&http, false, builder)
+        .await
+        .context("Failed to execute webhook")?;
+
+    println!("[INFO] Sent upload notification for {}", mp4_name);
     Ok(())
 }
